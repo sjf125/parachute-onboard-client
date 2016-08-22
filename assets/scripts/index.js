72e216e26793c63c65c6b8b02ed7f88e2ea3938d
sjf125 <sjf125@gmail.com>
2016-08-21 23:45:33 -0400

    Add some jquery for displaying forms section

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,14 +1,7 @@
 'use strict';
 
 $(() => {
-  // jQuery for page scrolling feature - requires jQuery Easing plugin
-  // $(function() {
-  //   $('a.page-scroll').bind('click', function(event) {
-  //       let $anchor = $(this);
-  //       $('html, body').stop().animate({
-  //           scrollTop: $($anchor.attr('href')).offset().top
-  //       }, 1500, 'easeInOutExpo');
-  //       event.preventDefault();
-  //   });
-  // });
+  $('#has-receipt').on('click', function() {
+    $('#forms').removeClass('hidden');
+  });
 });
